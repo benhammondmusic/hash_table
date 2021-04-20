3a1d652164fb1b0c322a8b71de6468919d981a85
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-04-20 15:54:18 -0600

    adds readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Building a Hash Map in Python
+
+Read details on my [blog](https://blog.benhammond.tech/build-a-hash-map-in-python).
--- test.py
+++ test.py
@@ -6,14 +6,13 @@ TABLE_SIZE = int(len(data) * 1.3)
 bens_table = Hash_Map(TABLE_SIZE)
 print("\033c")
 
-# bens_table.insert("Ben")
 for item in data:
-    # print(f"inserting:{item}",end=' ')
     bens_table.insert(item)
 print("\n")
 
 print(bens_table)
 
+print("---")
 
 print("finding Julie at", bens_table.search("Julie"))
 print("finding 1 at", bens_table.search("1"))
@@ -27,5 +26,3 @@ print("removing C:", bens_table.remove("C"))
 print("removing C:", bens_table.remove("B"))
 print("removing A:", bens_table.remove("A"))
 print("removing A:", bens_table.remove("C"))
-
-# print(bens_table)
