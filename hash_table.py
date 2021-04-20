6892b92a572d32f443f5773fdc215e5a3517b350
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-04-19 21:35:11 -0600

    simple hashmap setup with chaining for collisions using string concat

---
+++ hash_table.py
@@ -0,0 +1,25 @@
+TABLE_SIZE = 20
+data = ["Ben", "Julie", "Nora", "Emma", "A", "B", "C", "D", "1", "2", "3", "qwerasdfxcv", "1234qwersdfxzcv"]
+
+
+
+
+def hash(value):
+    hash_index = 0
+    for char in value:
+        hash_index += ord(char)
+    return hash_index % TABLE_SIZE
+
+hash_table = [None] * TABLE_SIZE
+
+
+for item in data:
+    hashed_index = hash(item)
+    print(f"ITEM: {item} INDEX: {hashed_index}")
+    if hash_table[hashed_index] == None:
+        hash_table[hashed_index] = item
+    else:
+        hash_table[hashed_index] = hash_table[hashed_index] + " " + item
+
+
+print("HASH TABLE", hash_table)
